ead62f7f7dc12af5ea1b49a0d0147a7273852dab
jssatorres <juansataz.dev@gmail.com>
2022-12-18 13:29:13 +0100

    feat (clients) controlers CRUD

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,7 +14,8 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        $clients = Client::all();
+        return response()->json($clients);
     }
 
     /**
@@ -36,6 +37,18 @@ class ClientController extends Controller
     public function store(Request $request)
     {
         //
+        $client = new Client;
+        $client->name = $request->name;
+        $client->email = $request->email;
+        $client->phone = $request->phone;
+        $client->address = $request->address;
+        $client->save();
+        $data = [
+            'message' => 'Client created succesfully',
+            'client' => $client
+        ];
+
+        return response()->json('$data');
     }
 
     /**
@@ -46,7 +59,7 @@ class ClientController extends Controller
      */
     public function show(Client $client)
     {
-        //
+        return response()->json('$client');
     }
 
     /**
@@ -69,7 +82,17 @@ class ClientController extends Controller
      */
     public function update(Request $request, Client $client)
     {
-        //
+        $client = new Client;
+        $client->name = $request->name;
+        $client->email = $request->email;
+        $client->phone = $request->phone;
+        $client->address = $request->address;
+        $client->save();
+        $data = [
+            'message' => 'Client updated succesfully',
+            'client' => $client
+        ];
+        return response()->json('$data');
     }
 
     /**
@@ -80,6 +103,11 @@ class ClientController extends Controller
      */
     public function destroy(Client $client)
     {
-        //
+        $client->delete();
+        $data = [
+            'message' => 'Client deleted succesfully',
+            'client' => $client
+        ];
+        return response()->json('$data');
     }
 }
